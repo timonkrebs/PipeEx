65d5a54d1d05affae55ec59e0edbb3e53eb75cd9
Timon <timonkrebs@hotmail.com>
2025-02-22 23:14:35 +0000

    added tests

--- PipeEx.Tests/UnitTest1.cs
+++ PipeEx.Tests/UnitTest1.cs
@@ -1,4 +1,3 @@
-using System.Runtime.CompilerServices;
 using PipeEx;
 using static BunsenBurner.ArrangeActAssert;
 
@@ -14,11 +13,82 @@ public class UnitTest1
     [Fact]
     public Task Test2() => Arrange(() => 1)
         .Act(x => x.I(y => Task.FromResult(y)))
-        .Assert(r => Assert.Equal(1, 1));
+        .Assert(r => Assert.Equal(1, r));
 
     [Fact]
     public Task Test3() => Arrange(() => 1)
         .Act(x => x.I(y => Task.FromResult(y))
-                   .I(y => y + 2))
+            .I(y => y + 2))
         .Assert(r => Assert.Equal(3, r));
+
+    [Fact]
+    public Task Test4_TupleDestructuring() => Arrange(() => (1, 2))
+        .Act(x => x.I((a, b) => a + b))
+        .Assert(r => Assert.Equal(3, r));
+
+    [Fact]
+    public Task Test5_TupleDestructuringAsync() => Arrange(() => (1, 2))
+        .Act(x => x.I(async (a, b) => await Task.FromResult(a + b)))
+        .Assert(r => Assert.Equal(3, r));
+
+    [Fact]
+    public Task Test6_TupleDestructuringTaskSource() => Arrange(() => Task.FromResult((1, 2)))
+        .Act(x => x.I((a, b) => a + b))
+        .Assert(r => Assert.Equal(3, r));
+
+    [Fact]
+    public Task Test7_TupleDestructuringTaskSourceAsync() => Arrange(() => Task.FromResult((1, 2)))
+        .Act(x => x.I(async (a, b) => await Task.FromResult(a + b)))
+        .Assert(r => Assert.Equal(3, r));
+
+    [Fact]
+    public Task Test8_TupleReturn() => Arrange(() => 1)
+        .Act(x => x.I(a => a + 1, a => a * 2))
+        .Assert(r => Assert.Equal((2, 2), r));
+
+    [Fact]
+    public Task Test9_TupleReturnAsync() => Arrange(() => Task.FromResult(1))
+        .Act(x => x.I(a => a + 1, a => Task.FromResult(a * 2)))
+        .Assert(r => Assert.Equal((2, 2), r));
+
+    [Fact]
+    public Task Test10_TupleReturnMixedAsync1() => Arrange(() => 1)
+        .Act(x => x.I(a => Task.FromResult(a + 1), a => a * 2))
+        .Assert(r => Assert.Equal((2, 2), r));
+
+    [Fact]
+    public Task Test11_TupleReturnMixedAsync2() => Arrange(() => 1)
+        .Act(x => x.I(a => a + 1, a => Task.FromResult(a * 2)))
+        .Assert(r => Assert.Equal((2, 2), r));
+
+    [Fact]
+    public Task Test12_TupleReturnMixedAsync3() => Arrange(() => 1)
+        .Act(x => x.I(a => Task.FromResult(a + 1), a => Task.FromResult(a * 2)))
+        .Assert(r => Assert.Equal((2, 2), r));
+
+
+    [Fact]
+    public Task Test13_TupleReturnTupleDestructuring() => Arrange(() => (1, 2))
+        .Act(x => x.I((a, b) => a + 1, (a, b) => b * 2))
+        .Assert(r => Assert.Equal((2, 4), r));
+
+    [Fact]
+    public Task Test14_TupleReturnTupleDestructuringAsync() => Arrange(() => Task.FromResult((1, 2)))
+        .Act(x => x.I((a, b) => a + 1, (a, b) => Task.FromResult(b * 2)))
+        .Assert(r => Assert.Equal((2, 4), r));
+
+    [Fact]
+    public Task Test15_TupleReturnTupleDestructuringMixedAsync1() => Arrange(() => (1, 2))
+        .Act(x => x.I((a, b) => Task.FromResult(a + 1), (a, b) => b * 2))
+        .Assert(r => Assert.Equal((2, 4), r));
+
+    [Fact]
+    public Task Test16_TupleReturnTupleDestructuringMixedAsync2() => Arrange(() => (1, 2))
+       .Act(x => x.I((a, b) => a + 1, (a, b) => Task.FromResult(b * 2)))
+       .Assert(r => Assert.Equal((2, 4), r));
+
+    [Fact]
+    public Task Test17_TupleReturnTupleDestructuringMixedAsync3() => Arrange(() => (1, 2))
+       .Act(x => x.I((a, b) => Task.FromResult(a + 1), (a, b) => Task.FromResult(b * 2)))
+       .Assert(r => Assert.Equal((2, 4), r));
 }
