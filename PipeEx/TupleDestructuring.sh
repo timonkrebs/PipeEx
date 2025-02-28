a869357ba7708a35aecfd17feac8c3c1e252f2d9
Timon <timonkrebs@hotmail.com>
2025-02-28 00:38:41 +0000

    added script to generate TupleDestructuring

---
+++ PipeEx/TupleDestructuring.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+cat << EOF > TupleDestructuring.cs
+namespace PipeEx;
+
+public static class TupleDestructuring
+{
+EOF
+for item in $(seq 1 12); do
+    ty="TSource1"
+    tv="source.Item1"
+
+    for i in $(seq 1 $item); do
+        ty="${ty}, TSource$((i + 1))"
+        tv="${tv}, source.Item$((i + 1))"
+    done
+
+    echo "Current item: $item, Types: $ty"
+
+cat << EOF >> TupleDestructuring.cs
+    public static TResult I<$ty, TResult>(this ($ty) source, Func<$ty, TResult> func)
+    {
+        return func($tv);
+    }
+
+    public static async Task<TResult> I<$ty, TResult>(this ($ty) source, Func<$ty, Task<TResult>> func)
+    {
+        return await func($tv);
+    }
+
+    public static async Task<TResult> I<$ty, TResult>(this Task<($ty)> s, Func<$ty, TResult> func)
+    {
+        var source = await s;
+        return func($tv);
+    }
+
+    public static async Task<TResult> I<$ty, TResult>(this Task<($ty)> s, Func<$ty, Task<TResult>> func)
+    {
+        var source = await s;
+        return await func($tv);
+    }
+EOF
+
+done
+cat << EOF >> TupleDestructuring.cs
+}
+EOF
